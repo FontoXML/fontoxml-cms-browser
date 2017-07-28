9a1042f52e3e3df1e7b7645060da714cc8a7e3dc
Stef Busking <stef.busking@gmail.com>
2017-07-28 12:33:07 +0200

    Disable Angular code

--- src/install.js
+++ src/install.js
@@ -3,20 +3,10 @@ define([
 
 	'./data-providers/dataProviders',
 	'./data-providers/createDataProviderUsingConfiguredConnectors',
-
-	'./modals/documents/FxCreateDocumentFormModalStackController',
 	'./modals/documents/FxCreateDocumentFormModalStack.jsx',
-
-	'./modals/documents/FxOpenDocumentBrowserModalController',
 	'./modals/documents/FxOpenDocumentBrowserModal.jsx',
-
-	'./modals/documents/FxOpenOrCreateDocumentModalStackController',
 	'./modals/documents/FxOpenOrCreateDocumentModalStack.jsx',
-
-	'./modals/document-links/FxDocumentLinkBrowserModalController',
 	'./modals/document-links/FxDocumentLinkBrowserModal.jsx',
-
-	'./modals/images/FxImageBrowserModalController',
 	'./modals/images/FxImageBrowserModal.jsx'
 ], function (
 	uiManager,
@@ -24,37 +14,23 @@ define([
 	dataProviders,
 	createDataProviderUsingConfiguredConnectors,
 
-	FxCreateDocumentFormModalStackController,
 	FxCreateDocumentFormModalStack,
-
-	FxOpenDocumentBrowserModalController,
 	FxOpenDocumentBrowserModal,
-
-	FxOpenOrCreateDocumentModalStackController,
 	FxOpenOrCreateDocumentModalStack,
-
-	FxDocumentLinkBrowserModalController,
 	FxDocumentLinkBrowserModal,
-
-	FxImageBrowserModalController,
 	FxImageBrowserModal
 ) {
 	'use strict';
 
 	return function install () {
-		uiManager.addController('FxCreateDocumentFormModalStackController', FxCreateDocumentFormModalStackController);
 		uiManager.registerReactComponent('FxCreateDocumentFormModalStack', FxCreateDocumentFormModalStack);
 
-		uiManager.addController('FxOpenDocumentBrowserModalController', FxOpenDocumentBrowserModalController);
 		uiManager.registerReactComponent('FxOpenDocumentBrowserModal', FxOpenDocumentBrowserModal);
 
-		uiManager.addController('FxOpenOrCreateDocumentModalStackController', FxOpenOrCreateDocumentModalStackController);
 		uiManager.registerReactComponent('FxOpenOrCreateDocumentModalStack', FxOpenOrCreateDocumentModalStack);
 
-		uiManager.addController('FxDocumentLinkBrowserModalController', FxDocumentLinkBrowserModalController);
 		uiManager.registerReactComponent('FxDocumentLinkBrowserModal', FxDocumentLinkBrowserModal);
 
-		uiManager.addController('FxImageBrowserModalController', FxImageBrowserModalController);
 		uiManager.registerReactComponent('FxImageBrowserModal', FxImageBrowserModal);
 
 		dataProviders.set(
