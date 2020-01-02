ae5716cde4cf186ee205dd3d5779941b196344b6
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2020-01-02 12:14:12 +0100

    Replace direct import of glamor with FDS' applyCss.

--- src/images/ImagePreview.jsx
+++ src/images/ImagePreview.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-import { merge } from 'glamor';
-
 import {
 	Flex,
 	Heading,
@@ -11,19 +9,22 @@ import {
 	SpinnerIcon,
 	StateMessage
 } from 'fds/components';
-import { block } from 'fds/system';
+import { applyCss, block } from 'fds/system';
 import FxImageLoader from 'fontoxml-fx/src/FxImageLoader.jsx';
 
-const imageStyles = merge(block, {
-	position: 'absolute',
-	maxWidth: '100%',
-	maxHeight: '100%',
-	width: 'auto',
-	height: 'auto',
-	top: '50%',
-	left: '50%',
-	transform: 'translateX(-50%) translateY(-50%)'
-});
+const imageStyles = applyCss([
+	block,
+	{
+		position: 'absolute',
+		maxWidth: '100%',
+		maxHeight: '100%',
+		width: 'auto',
+		height: 'auto',
+		top: '50%',
+		left: '50%',
+		transform: 'translateX(-50%) translateY(-50%)'
+	}
+]);
 
 class ImagePreview extends Component {
 	static propTypes = {
